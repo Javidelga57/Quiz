267335896a3741afc240c8bccdae396c89c31624
Javidelga57 <Javier.delgadopalacios@gmail.com>
2015-04-30 11:22:16 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var quizController = require('/home/javier.delgado.palacios/mis_proyectos/quiz/controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+router.get('quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
