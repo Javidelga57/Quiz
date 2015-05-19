126404f5acb0f3932b26d9a73e91daf83426851d
Javidelga57 <Javier.delgadopalacios@gmail.com>
2015-05-19 16:35:00 +0200

    Despliegue DB en Heroku paso 11

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,53 @@
 var models = require('../models/models.js');
 
-//GET /quizes/question
-exports.question=function(req, res){
-	models.Quiz.findAll().success(function(quiz) {
-	res.render('quizes/question', {pregunta: quiz[0].pregunta})
-})
+//Autoload - factoriza el código si ruta incluye:quizId
+exports.load =function(req,res,next,quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else { next(new Error('No existe quizId=' +quizId));}
+		}
+	).catch(function(error) { next(error);});
 };
-//GET /quizes/answer
+
+//Get /quizes
+exports.index = function(req,res) {
+	models.Quiz.findAll().then(
+		function(quizes) {
+			res.render('quizes/index', {quizes:quizes});
+		}
+	).catch(function(error) { next(error);})
+};
+//GET /quizes/new
+exports.new = function(req,res) {
+	var quiz = models.Quiz.build( //crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new', {quiz:quiz});
+};
+
+//GET /quizes/:id
+exports.show=function(req, res){
+	res.render('quizes/show', {quiz: req.quiz});
+};
+
+//GET /quizes/:id/answer
 exports.answer=function(req, res){
-	models.Quiz.findAll().success(function(quiz) {
-	if(req.query.respuesta === quiz[0].respuesta){
-	res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-	res.render('quizes/answer',{respuesta:'Incorrecto'});
+	var resultado ='Incorrecto';
+	if(req.query.respuesta === req.quiz.respuesta) {
+		resultado ='Correcto';
 	}
-})
+	res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+
+// guarda en DB los campos pregunta y respuesta de quiz
+quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+	res.redirect('/quizes');
+})  //Redirección HTTP (URL relativo) lista de preguntas
 };
